35fd36f9c195997afcfcb09c786ecbfcb9c4ccce
raikevichI <119799261+raikevichI@users.noreply.github.com>
2023-03-26 23:10:16 +0300

    Update dz_2.cpp
    Deleate code about brakets

--- dz_2/dz_2.cpp
+++ dz_2/dz_2.cpp
@@ -5,46 +5,8 @@
 #include "stack.hpp"
 #include <string>
 
-bool checkBalanceBrackets(const std::string& text, const std::size_t maxDeep)
-{
-    StackArray<char> brackets(text.size());
-
-    for (char c : text) {
-        char openBracket = '\0';
-        switch (c) {
-        case '(':
-        case '[':
-        case '{':
-            if (brackets.full())
-                return false;
-            openBracket = c;
-            brackets.push(c);
-            break;
-        case ')':
-            openBracket = '(';
-            if (brackets.isEmpty() || brackets.pop() != openBracket)
-                return false;
-            break;
-        case ']':
-            openBracket = '[';
-            if (brackets.isEmpty() || brackets.pop() != openBracket)
-                return false;
-            break;
-        case '}':
-            openBracket = '{';
-            if (brackets.isEmpty() || brackets.pop() != openBracket)
-                return false;
-            break;
-        default:
-            break;
-        }
-    }
-
-    return brackets.isEmpty();
-}
 int main()
 {
-    //std::cout << checkBalanceBrackets("(x(x{[x]}x)x)", 9) << std::endl;
     StackArray<int> stack1(6);
     stack1.push(1);
     stack1.push(2);
